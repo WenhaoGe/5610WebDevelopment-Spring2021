0c0418c9b29a4efd1eded6c76d3e1a39002fcbec
Wenhao Ge <ge.wen@northeastern.edu>
2021-01-22 19:24:44 -0800

    Created log in page

---
+++ src/main/java/com/example/wbdvspring2021wenhaoserverjava/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbdvspring2021wenhaoserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
